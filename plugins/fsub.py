07bb76037dfd19f80d817f99b6b0e7b60c383925
ʜᴀʀɪ <127658855+HS-BOTZ@users.noreply.github.com>
2023-10-25 23:52:39 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -102,10 +102,7 @@ async def ForceSub(bot: Client, update: Message, file_id: str = False, mode="che
                 InlineKeyboardButton("🎗 Rᴇǫᴜᴇꜱᴛ Tᴏ Jᴏɪɴ Cʜᴀɴɴᴇʟ 🎗", url=invite_link)
             ],
             [
-                InlineKeyboardButton("㋡ Wʜʏ I'ᴍ Jᴏɪɴɪɴɢ", callback_data='whyjoin')
-            ],
-            [
-                InlineKeyboardButton("📥 Mᴇ Jᴏɪɴᴇᴅ 📥", callback_data=f"{mode}#{file_id}")
+                InlineKeyboardButton("❤️ Try Now ❤️ ", callback_data=f"{mode}#{file_id}")
             ]
         ]
         
