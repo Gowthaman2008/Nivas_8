c4c046b5c6c1b247b8387cb46cafec33481d0d96
Gowthaman2008 <110714953+Gowthaman2008@users.noreply.github.com>
2023-10-27 20:33:54 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -37,7 +37,7 @@ class script(object):
 
 <code>/batch https://t.me/movies_tamizhaass https://t.me/movies_tamizhaass</code>
 
-𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ››  <a href=https://t.me/GM_Botzz> Gᴍ Bᴏᴛᴢᴢ™</a>"""
+𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ››  <a href=https://t.me/NMOAdda> NMOAdda</a>"""
     
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
@@ -83,7 +83,7 @@ class script(object):
 ›› /set_template - 𝚂𝙴𝚃 𝙲𝚄𝚂𝚃𝙾𝙼 𝙸𝙼𝙳𝙱 𝚃𝙴𝙼𝙿𝙻𝙰𝚃𝙴 𝙵𝙾𝚁 𝙰𝚄𝚃𝙾 𝙵𝙸𝙻𝚃𝙴𝚁. 
 ›› /get_template - 𝙶𝙴𝚃 𝙲𝚄𝚁𝚁𝙴𝙽𝚃 𝙸𝙼𝙳𝙱 𝚃𝙴𝙼𝙿𝙻𝙰𝚃𝙴 𝙾𝙵 𝙰𝚄𝚃𝙾 𝙵𝙸𝙻𝚃𝙴𝚁.
 
-𝙲𝚁𝙴𝙳𝙸𝚃𝚂 :- <a href=https://t.me/GM_Botzz> Gᴍ Bᴏᴛᴢᴢ™</a>"""
+𝙲𝚁𝙴𝙳𝙸𝚃𝚂 :- <a href=https://t.me/NMOAdda> NMOAdda</a>"""
 
     CONNECTION_TXT = """Help: <b>Connections</b>
 
@@ -195,7 +195,7 @@ By @{} """
 
 ➪ 𝖩𝗎𝗌𝗍 𝗌𝖾𝗇𝖽 𝗆𝖾 𝖺 𝗂𝗆𝖺𝗀𝖾 𝗍𝗈 𝖾𝖽𝗂𝗍 ✨
 
-𝖬𝖺𝖽𝖾 𝖻𝗒 <a href=https://t.me/Gowthaman2008>𝙶𝚘𝚠𝚝𝚑𝚊𝚖𝚊𝚗</a>"""
+𝖬𝖺𝖽𝖾 𝖻𝗒 <a href=https://t.me/NivasMoviesOnlineAdda>Nivas 💥</a>"""
 
     RESTRIC_TXT = """➤ 𝐇𝐞𝐥𝐩: Mᴜᴛᴇ 🚫
 
