d15d44fc91bdb2b8926aabcc1950a16bac1ecff0
Gowthaman2008 <110714953+Gowthaman2008@users.noreply.github.com>
2024-04-29 15:17:24 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -529,7 +529,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton("🎈 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴩ 🎈", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
             InlineKeyboardButton(' NMOAdda ', url='https://t.me/NMOAdda'), 
-            InlineKeyboardButton("🎄 Gʀᴏᴜᴩ 🎄", url="https://t.me/New_Movies_Request_Groups")
+            InlineKeyboardButton("🎄 Gʀᴏᴜᴩ 🎄", url="https://t.me/Movie_Request_Groups_Hd")
             ],[
             InlineKeyboardButton("⚙️ Hᴇʟᴩ ⚙️", callback_data="help"),
             InlineKeyboardButton("✨ Aʙᴏᴜᴛ ✨", callback_data="about")
